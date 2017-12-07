f61ac259ecd0bece0363d53851374e77c4b38cdf
tomasreichmann <tomasreichmann@gmail.com>
2017-12-07 17:21:36 +0100

    remove .next

--- .next/bundles/pages/produkty.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/produkty', function() {
-            var comp = module.exports=webpackJsonp([6],{234:function(e,t,u){e.exports=u(235)},235:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=u(1),r=l(n),a=u(32),d=l(a),o=u(33),f=l(o),c=function(){return r.default.createElement(d.default,{title:"produkty"},r.default.createElement(f.default,null,r.default.createElement("h1",null,"Produkty")))};t.default=c}},[234]);
-            return { page: comp.default }
-          })
-        
