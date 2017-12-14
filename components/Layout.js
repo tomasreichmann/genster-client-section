3f3311d2c68c5366384ccf069be9acdf087d69ec
tomasreichmann <tomasreichmann@gmail.com>
2017-12-14 11:13:11 +0100

    moved styles to Layout and removed custom _document.js

--- components/Layout.js
+++ components/Layout.js
@@ -4,8 +4,60 @@ import PropTypes from 'prop-types'
 import theme from '../constants/theme'
 
 const Layout = ({ children, title = 'Genster - klientská sekce' }) => (
-  <div>
-    <style jsx>{`
+  <div className="page">
+    <style global jsx>{`
+      @font-face {
+        font-family: Genster-Montserrat;
+        font-style: normal;
+        font-weight: 100;
+        src: url(/static/fonts/montserrat-100.eot);
+        src: url(/static/fonts/montserrat-100.woff2) format("woff2"),url(/static/fonts/montserrat-100.woff) format("woff"),url(/static/fonts/montserrat-100.ttf) format("truetype")
+      }
+
+      @font-face {
+        font-family: Genster-Montserrat;
+        font-style: normal;
+        font-weight: 300;
+        src: url(/static/fonts/montserrat-300.eot);
+        src: url(/static/fonts/montserrat-300.woff2) format("woff2"),url(/static/fonts/montserrat-300.woff) format("woff"),url(/static/fonts/montserrat-300.ttf) format("truetype")
+      }
+
+      @font-face {
+        font-family: Genster-Montserrat;
+        font-style: normal;
+        font-weight: 500;
+        src: url(/static/fonts/montserrat-500.eot);
+        src: url(/static/fonts/montserrat-500.woff2) format("woff2"),url(/static/fonts/montserrat-500.woff) format("woff"),url(/static/fonts/montserrat-500.ttf) format("truetype")
+      }
+
+      body {
+        background: ${theme.color.paperInverted};
+        font-family: ${theme.font.text};
+        font-size: ${theme.font.size.text};
+        font-weight: ${theme.font.weight.text};
+        color: ${theme.color.textInverted};
+        margin: 0;
+        padding: 0;
+      }
+
+      .title {
+        font-family: ${theme.font.text};
+        font-size: ${theme.font.size.title};
+        font-weight: ${theme.font.weight.title};
+        color: ${theme.color.textInverted};
+      }
+
+      h1 {
+        font-family: ${theme.font.text};
+        font-size: ${theme.font.size.heading};
+        font-weight: ${theme.font.weight.heading};
+        color: ${theme.color.primary};
+      }
+
+      .page {
+        padding: ${theme.spacing.horizontalBig}px ${theme.spacing.verticalBig}px;
+      }
+
       .logo {
         height: ${theme.font.size.text};
       }
@@ -24,12 +76,23 @@ const Layout = ({ children, title = 'Genster - klientská sekce' }) => (
         font-weight: ${theme.font.weight.light};
         text-align: center;
       }
+
+      @media all and (min-width: ${theme.responsive.desktop}) {
+        .page {
+          width: ${theme.responsive.desktop};
+          margin: 0 auto;
+        }
+      }
+    `}</style>
+    <style jsx>{`
+
     `}</style>
     <Head>
       <title>{title}</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <link rel='shortcut icon' type='image/x-icon' href='/static/favicon.svg' />
+
     </Head>
     <header>
       <nav>
